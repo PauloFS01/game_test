32c23d6ab68bed32960178c489bad82dfb4b6220
Paulo <paulofs10.sea@gmail.com>
2022-07-08 00:01:53 -0300

    first log

---
+++ Game test/Assets/cube.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("That's first print of this project!");
+        Debug.LogWarning("That's first warning of this project!");
+        Debug.LogError("That's first error of this project!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
